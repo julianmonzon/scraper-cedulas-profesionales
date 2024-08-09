82b88551621563d5501c3f6810e25bdeb2439a0a
julianvazquezm <julian.vazquez@medtrainer.com>
2024-08-09 10:59:11 -0600

    deleted superfluo comments

--- cedulas_profesionales/spiders/cedulas_profesionales.py
+++ cedulas_profesionales/spiders/cedulas_profesionales.py
@@ -2,6 +2,7 @@ import os
 from scrapy.http import FormRequest, Request
 from cedulas_profesionales.spiders.base import BaseSpider
 
+
 class CedulasProfesionalesSpider(BaseSpider):
     name = "cedulas_profesionales"
 
@@ -13,7 +14,7 @@ class CedulasProfesionalesSpider(BaseSpider):
         self.nombre = nombre
         self.paterno = paterno
         self.materno = materno
-        self.results = None  # Asegúrate de definir el atributo results
+        self.results = None
 
     def start_requests(self):
         """Start the scraping process by checking the availability of the website."""
@@ -28,7 +29,8 @@ class CedulasProfesionalesSpider(BaseSpider):
         yield init_request
 
     def search_cedula(self, response):
-        """Initiate a cedula search by submitting a form with specific search criteria."""
+        """Initiate a cedula search by submitting a form with specific
+        search criteria."""
         self.logger.info(f"The website {response.request.url} is up and running.")
 
         formdata = {
@@ -46,7 +48,7 @@ class CedulasProfesionalesSpider(BaseSpider):
             callback=self.extract_cedula_records,
             errback=self.errback_httpbin,
         )
-        
+
         yield request
 
     def extract_cedula_records(self, response):
@@ -61,20 +63,20 @@ class CedulasProfesionalesSpider(BaseSpider):
                 self.results = "No se encontraron resultados"
             case _:
                 self.results = self.extract_data(rows)
-        
-        self.save_results()  # Guarda los resultados en un archivo
+
+        self.save_results()
 
     def extract_record(self, row):
         """Extract details from a single row in the search results page."""
         try:
             return {
-                'cedula': self.clean_text(row.xpath('.//th[1]/a/text()').get()),
-                'tipo': self.clean_text(row.xpath('.//td[1]/text()').get()),
-                'nombre_completo': self.clean_text(row.xpath('.//td[2]/text()').get()),
-                'carrera': self.clean_text(row.xpath('.//th[2]/text()').get()),
-                'universidad': self.clean_text(row.xpath('.//td[3]/text()').get()),
-                'estado': self.clean_text(row.xpath('.//th[3]/text()').get()),
-                'año': self.clean_text(row.xpath('.//td[4]/text()').get())
+                "cedula": self.clean_text(row.xpath(".//th[1]/a/text()").get()),
+                "tipo": self.clean_text(row.xpath(".//td[1]/text()").get()),
+                "nombre_completo": self.clean_text(row.xpath(".//td[2]/text()").get()),
+                "carrera": self.clean_text(row.xpath(".//th[2]/text()").get()),
+                "universidad": self.clean_text(row.xpath(".//td[3]/text()").get()),
+                "estado": self.clean_text(row.xpath(".//th[3]/text()").get()),
+                "año": self.clean_text(row.xpath(".//td[4]/text()").get()),
             }
         except Exception as e:
             self.logger.error(f"Error extracting record: {e}")
@@ -92,9 +94,8 @@ class CedulasProfesionalesSpider(BaseSpider):
 
     def save_results(self):
         """Save the results to a text file."""
-        results_path = os.path.join(os.getcwd(), 'results.txt')
-        with open(results_path, 'w', encoding='utf-8') as f:
-            # Asegúrate de que self.results sea un diccionario
+        results_path = os.path.join(os.getcwd(), "results.txt")
+        with open(results_path, "w", encoding="utf-8") as f:
             if isinstance(self.results, dict):
                 for idx, record in self.results.items():
                     if isinstance(record, dict):
@@ -106,5 +107,4 @@ class CedulasProfesionalesSpider(BaseSpider):
                         f.write(f"Registro {idx + 1}: {record}\n")
                         f.write("\n")
             else:
-                # Imprime el contenido de self.results si no es un diccionario
                 f.write(f"{self.results}\n")
